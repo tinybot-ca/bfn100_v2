6c6608b15cbce706f376d7fc948eb465a99946bd
Michael Whang <mike@tinybot.ca>
2018-04-16 22:22:08 -0700

    Record successful login to Activity table

--- app/Listeners/LogSuccessfulLogin.php
+++ app/Listeners/LogSuccessfulLogin.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Activity;
 
 class LogSuccessfulLogin implements ShouldQueue
 {
@@ -27,7 +28,11 @@ class LogSuccessfulLogin implements ShouldQueue
     public function handle(Login $event)
     {
         $geoplugin =  var_export(unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip=' . request()->ip())));
-        dd($geoplugin);
-        dd('User with this email: ' . $event->user->email . ' just logged in!');
+        // dd($geoplugin);
+        // dd('User with this email: ' . $event->user->email . ' just logged in!');
+
+        Activity::create([
+            'description' => 'Username: ' . $event->user->name . ' | Email: ' . $event->user->email . ' | IP: ' . request()->ip()
+        ]);
     }
 }
