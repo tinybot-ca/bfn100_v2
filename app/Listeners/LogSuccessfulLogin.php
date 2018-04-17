8c1d4037f0bdc8dcb8777ef26bec432b3c2e8420
tinybot <mike@tinybot.ca>
2018-04-16 20:13:09 -0700

    Experiment with IP logging

--- app/Listeners/LogSuccessfulLogin.php
+++ app/Listeners/LogSuccessfulLogin.php
@@ -6,7 +6,7 @@ use Illuminate\Auth\Events\Login;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class LogSuccessfulLogin
+class LogSuccessfulLogin implements ShouldQueue
 {
     /**
      * Create the event listener.
@@ -26,6 +26,8 @@ class LogSuccessfulLogin
      */
     public function handle(Login $event)
     {
+        $geoplugin =  var_export(unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip=' . request()->ip())));
+        dd($geoplugin);
         dd('User with this email: ' . $event->user->email . ' just logged in!');
     }
 }
