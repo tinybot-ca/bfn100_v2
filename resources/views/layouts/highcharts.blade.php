0294a01e47a9e0a6f24291b8126344147fd9a7fd
tinybot <mike@tinybot.ca>
2018-04-23 08:15:51 -0700

    Update stacked area chart

--- resources/views/layouts/highcharts.blade.php
+++ resources/views/layouts/highcharts.blade.php
@@ -192,36 +192,63 @@
         function drawMyChart(categories, series) {
             var myChart = Highcharts.chart('rollingHistory', {
 
-                credits: {
-                    enabled: false
-                },
                 chart: {
-                    type: 'line'
+                    type: 'area'
                 },
                 title: {
-                    text: 'Current Month'
+                    text: 'Last 6 Months'
                 },
                 subtitle: {
-                    text: 'Total push-ups for {{ date('F') }}'
+                    text: 'Rolling 6 month history'
                 },
                 xAxis: {
-                    categories: categories
+                    categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+                    tickmarkPlacement: 'on',
+                    title: {
+                        enabled: false
+                    }
                 },
                 yAxis: {
                     title: {
-                        text: 'Push-ups'
+                        text: 'Billions'
+                    },
+                    labels: {
+                        formatter: function () {
+                            return this.value / 1000;
+                        }
                     }
+                },
+                tooltip: {
+                    split: true,
+                    valueSuffix: ' millions'
                 },
                 plotOptions: {
-                    line: {
-                        dataLabels: {
-                            enabled: false
-                        },
-                        enableMouseTracking: true
+                    area: {
+                        stacking: 'normal',
+                        lineColor: '#666666',
+                        lineWidth: 1,
+                        marker: {
+                            lineWidth: 1,
+                            lineColor: '#666666'
+                        }
                     }
                 },
-                type: 'category',
-                series: series
+                series: [{
+                    name: 'Asia',
+                    data: [502, 635, 809, 947, 1402, 3634, 5268]
+                }, {
+                    name: 'Africa',
+                    data: [106, 107, 111, 133, 221, 767, 1766]
+                }, {
+                    name: 'Europe',
+                    data: [163, 203, 276, 408, 547, 729, 628]
+                }, {
+                    name: 'America',
+                    data: [18, 31, 54, 156, 339, 818, 1201]
+                }, {
+                    name: 'Oceania',
+                    data: [2, 2, 2, 6, 13, 30, 46]
+                }]
 
             }); // Highcharts options
         }
