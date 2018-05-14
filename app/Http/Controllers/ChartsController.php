7886d61c6f7d6d874fd1b40db27084b57dd3a603
Michael Whang <mike@tinybot.ca>
2018-05-14 12:37:02 -0700

    Increase word cloud fetch limit

--- app/Http/Controllers/ChartsController.php
+++ app/Http/Controllers/ChartsController.php
@@ -171,7 +171,7 @@ class ChartsController extends Controller
     public function wordCloud()
     {
         $chart = '';
-        $pushups = Pushup::latest()->limit(50)->get();
+        $pushups = Pushup::latest()->limit(150)->get();
 
         foreach ($pushups as $pushup)
         {
