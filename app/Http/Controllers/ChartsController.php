c2ca72f1800fb606e5e1af24813ca6ca3bdbedc3
Michael Whang <mike@tinybot.ca>
2018-07-09 09:58:43 -0700

    Adjust word cloud fetch count

--- app/Http/Controllers/ChartsController.php
+++ app/Http/Controllers/ChartsController.php
@@ -171,7 +171,7 @@ class ChartsController extends Controller
     public function wordCloud()
     {
         $chart = '';
-        $pushups = Pushup::latest()->limit(20)->get();
+        $pushups = Pushup::latest()->limit(30)->get();
 
         foreach ($pushups as $pushup)
         {
